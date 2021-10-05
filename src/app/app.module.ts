51c5a34b24cf5efacba82ec4703dba4adf62bfe5
Ivan Lua <miguellua100@gmail.com>
2021-10-05 13:25:41 -0600

    Created first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
